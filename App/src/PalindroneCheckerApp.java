a83748005c7e2f0ae010c46b8ee9e1dfdf442197
Ayushdk1218 <ayushdk2005@gmail.com>
2026-02-23 15:37:27 +0530

    Ayush Dungrakoti Added string reverse

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -11,7 +11,6 @@ public class PalindroneCheckerApp {
         System.out.println("Welcome to Palindrome Checker App Management System");
         System.out.print("Enter a word or phrase: ");
 
-
         String input = scanner.nextLine();
 
         if (isPalindrome(input)) {
@@ -20,24 +19,19 @@ public class PalindroneCheckerApp {
             System.out.println("\"" + input + "\" is not a palindrome.");
         }
 
-
         scanner.close();
     }
 
     public static boolean isPalindrome(String str) {
+
         String cleanStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        int left = 0;
-        int right = cleanStr.length() - 1;
+        String reversed = "";
 
-        while (left < right) {
-            if (cleanStr.charAt(left) != cleanStr.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
+        for (int i = cleanStr.length() - 1; i >= 0; i--) {
+            reversed = reversed + cleanStr.charAt(i);
         }
 
-        return true;
+        return cleanStr.equals(reversed);
     }
 }
